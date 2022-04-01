45a51e7f32ee2514cc0ef642ae8d2e26be1783b0
Johannes Launer <johannes.launer@emmos.de>
2022-04-01 07:24:35 +0200

    added plugin to read content of files

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,17 +1,17 @@
 module.exports = {
   siteMetadata: {
-    title: `new gatsby-config`,
-    siteUrl: `https://www.yourdomain.tld`,
+    title: "My Super Cool Blog",
   },
   plugins: [
     "gatsby-plugin-image",
     "gatsby-plugin-sharp",
     {
-      resolve: "gatsby-source-filesystem",
+      resolve: `gatsby-source-filesystem`,
       options: {
         name: `blog`,
-        path: `${__dirname}/blog`,
+        path: `${__dirname}/blog/`,
       },
     },
+    "gatsby-plugin-mdx",
   ],
 };
